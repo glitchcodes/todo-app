5ec653d799b8935a6ad407b69deb1711ad44960b
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2023-11-17 16:09:56 +0800

    add design to ADD_EDIT_TODO

--- app/src/main/java/com/example/todoappcompose/ui/add_edit_todo/AddEditTodoScreen.kt
+++ app/src/main/java/com/example/todoappcompose/ui/add_edit_todo/AddEditTodoScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -22,11 +23,16 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.todoappcompose.ui.theme.Blue50
+import com.example.todoappcompose.ui.theme.GrandHotel
+import com.example.todoappcompose.ui.theme.MainBG
+import com.example.todoappcompose.ui.theme.Purple100
 import com.example.todoappcompose.util.UIEvent
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -54,60 +60,73 @@ fun AddEditScreen(
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) },
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                viewModel.onEvent(AddEditTodoEvent.OnSaveTodoClick)
-            },
-                modifier = Modifier.padding(bottom = 64.dp)
+            FloatingActionButton(
+                onClick = {
+                    viewModel.onEvent(AddEditTodoEvent.OnSaveTodoClick)
+                },
+                containerColor = Purple100,
+                modifier = Modifier.padding(bottom = 78.dp)
             ) {
-                Icon(imageVector = Icons.Default.Check, contentDescription = "Save")
+                Icon(imageVector = Icons.Default.Check, contentDescription = "Save", tint = Color.White)
             }
         },
         content = {
-            Column {
-
-                Spacer(modifier = Modifier.height(64.dp))
-
-                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                    Text(
-                        text = "Create Todo",
-                        fontSize = 40.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(120.dp))
-
-                Column(
-                    modifier = Modifier.fillMaxSize()
+            Surface(
+                color = MainBG,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier.padding(16.dp)
                 ) {
-                    TextField(
-                        value = viewModel.title,
-                        onValueChange = {
-                            viewModel.onEvent(AddEditTodoEvent.OnTitleChange(it))
-                        },
-                        label = {
-                            Text(text = "Title")
-                        },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(12.dp))
-                    TextField(
-                        value = viewModel.description,
-                        onValueChange = {
-                            viewModel.onEvent(AddEditTodoEvent.OnDescriptionChange(it))
-                        },
-                        label = {
-                            Text(text = "Description")
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        singleLine = false,
-                        maxLines = 5
-                    )
+                    Column {
+
+                        Spacer(modifier = Modifier.height(64.dp))
+
+                        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                            Text(
+                                text = when {
+                                    viewModel.id.isBlank() -> "Create a Todo"
+                                    else -> "Edit Todo"
+                                },
+                                fontFamily = GrandHotel,
+                                fontSize = 64.sp,
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Center,
+                                color = Blue50
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(120.dp))
+
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            TextField(
+                                value = viewModel.title,
+                                onValueChange = {
+                                    viewModel.onEvent(AddEditTodoEvent.OnTitleChange(it))
+                                },
+                                label = {
+                                    Text(text = "Title")
+                                },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(12.dp))
+                            TextField(
+                                value = viewModel.description,
+                                onValueChange = {
+                                    viewModel.onEvent(AddEditTodoEvent.OnDescriptionChange(it))
+                                },
+                                label = {
+                                    Text(text = "Description")
+                                },
+                                modifier = Modifier.fillMaxWidth(),
+                                singleLine = false,
+                                maxLines = 5
+                            )
+                        }
+                    }
                 }
             }
         }
